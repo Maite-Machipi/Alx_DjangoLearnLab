8819c979ab50af0650e6e04c9559727197e39e08
Maite <maitemarageni@gmail.com>
2026-01-17 14:39:15 +0200

    Update query_samples to match required ORM queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,16 @@
-# query_samples.py
-# Sample ORM queries demonstrating relationships
-
 from relationship_app.models import Author, Book, Library, Librarian
 
-
 def query_books_by_author(author_name):
-    """Query all books by a specific author."""
-    return Book.objects.filter(author__name=author_name)
-
+    # Query all books by a specific author
+    author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 def list_books_in_library(library_name):
-    """List all books in a library."""
+    # List all books in a library
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-
-def get_librarian_for_library(library_name):
-    """Retrieve the librarian for a library."""
+def retrieve_librarian_for_library(library_name):
+    # Retrieve the librarian for a library
     library = Library.objects.get(name=library_name)
     return Librarian.objects.get(library=library)
