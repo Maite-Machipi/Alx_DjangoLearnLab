4232d4ca3c93e4210636463e6805fe62292a2aad
Maite <maitemarageni@gmail.com>
2026-01-31 20:32:34 +0200

    Fix BookViewSet by importing viewsets

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('books/', BookList.as_view(), name='book-list'),
+
+    path('', include(router.urls)),
 ]
