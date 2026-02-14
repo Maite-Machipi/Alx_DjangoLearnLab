33ac079e145e37c25c6ce82c01f6cff697a2cecd
Maite <maitemarageni@gmail.com>
2026-02-14 10:10:55 +0200

    Add CRUD views, forms, templates for blog posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,9 +2,13 @@ from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 
 from .forms import RegisterForm, ProfileForm
-
+from .models import Post
+from .forms import PostForm
 
 def register_view(request):
     if request.method == "POST":
@@ -33,3 +37,45 @@ def profile_view(request):
         form = ProfileForm(instance=request.user)
 
     return render(request, "blog/profile.html", {"form": form})
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    ordering = ["-published_date"]
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = "/posts/"
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
