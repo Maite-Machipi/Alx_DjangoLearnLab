321b23132ce64728bbf46db737014209432405c5
Maite <maitemarageni@gmail.com>
2026-02-15 11:36:36 +0200

    api/views.py added permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 class BookListView(generics.ListAPIView):
@@ -10,7 +11,7 @@ class BookListView(generics.ListAPIView):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class BookDetailView(generics.RetrieveAPIView):
@@ -20,7 +21,7 @@ class BookDetailView(generics.RetrieveAPIView):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class BookCreateView(generics.CreateAPIView):
