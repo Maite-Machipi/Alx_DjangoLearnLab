8e363442b819f50d82d852931d69254b283281f6
Maite <maitemarageni@gmail.com>
2026-03-01 13:22:39 +0200

    Add requirements.txt

--- fitness-tracker-api/fitness_tracker/urls.py
+++ fitness-tracker-api/fitness_tracker/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("api.urls")),
+    path("api/v1/auth/token/", views.obtain_auth_token),
+    path("api-auth/", include("rest_framework.urls")),
+
 ]
